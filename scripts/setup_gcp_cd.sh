219f31662ec2e1d901a4e1e0263b9a1bdd8487f5
maufee <maufee@users.noreply.github.com>
2025-11-03 18:09:24 -0800

    fix(ci): Use flags to set build service account
    - Removes the incorrect alpha command from the setup script.
    - Uses the 'flags' input in the deploy-cloudrun action to correctly specify the build service account for the deployment.

--- scripts/setup_gcp_cd.sh
+++ scripts/setup_gcp_cd.sh
@@ -112,8 +112,7 @@ grant_roles() {
     fi
 
     grant_project_iam_binding "serviceAccount:$BUILD_SA_EMAIL" "roles/run.admin"
-    echo "Setting dedicated Cloud Build SA as the project default..."
-    gcloud alpha builds settings update --service-account="$BUILD_SA_EMAIL"
+    grant_project_iam_binding "serviceAccount:$BUILD_SA_EMAIL" "roles/iam.serviceAccountUser"
 
     echo "Setting dedicated Cloud Build SA as the project default..."
     gcloud config set builds/service_account "$BUILD_SA_EMAIL"
